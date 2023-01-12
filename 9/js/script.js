90a462d29795d62dd52d71cf9d8861e212da3cbb
Keks <keks@htmlacademy.ru>
2023-01-12 19:18:14 +0300

    :heavy_check_mark: Сборка #9

--- 9/js/script.js
+++ 9/js/script.js
@@ -1,16 +1,1 @@
-let nav = document.querySelector('.nav');
-let navToggle = document.querySelector('.nav__toggle');
-let headerLogo = document.querySelector('.header__logo');
-
-nav.classList.remove('nav--nojs');
-headerLogo.classList.remove('header__logo--nojs');
-
-navToggle.addEventListener('click', function () {
-  if (nav.classList.contains('nav--closed')) {
-    nav.classList.remove('nav--closed');
-    nav.classList.add('nav--opened');
-  } else {
-    nav.classList.add('nav--closed');
-    nav.classList.remove('nav--opened');
-  }
-});
+let nav=document.querySelector(".nav"),navToggle=document.querySelector(".nav__toggle"),headerLogo=document.querySelector(".header__logo");nav.classList.remove("nav--nojs"),headerLogo.classList.remove("header__logo--nojs"),navToggle.addEventListener("click",(function(){nav.classList.contains("nav--closed")?(nav.classList.remove("nav--closed"),nav.classList.add("nav--opened")):(nav.classList.add("nav--closed"),nav.classList.remove("nav--opened"))}));
